bc850c35656cf621fe725ccddb95441b00b1a574
kaoudis <kaoudis@colorado.edu>
2015-10-28 23:26:49 +0000

    add regular relay config

--- setup-container.sh
+++ setup-container.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 yum update && yum install epel-release && yum install vim && yum install tor
+chown root /var/lib/tor
+tor --list-fingerprint --orport 1 \
+    --dirserver "x 127.0.0.1:1 ffffffffffffffffffffffffffffffffffffffff" \
+    --datadirectory /var/lib/tor/
