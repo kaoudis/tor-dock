16fd6d4fbe35f38fe7ce84f83bb1a8a0e0e283cf
Kelly Nicole Kaoudis <kaoudis@colorado.edu>
2015-11-28 15:55:49 -0700

    update create scripty

--- create-container.sh
+++ create-container.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-sudo docker create -i -t --name=one --link=dirserver:dirserver ubuntu:latest /bin/bash
-sudo docker create -i -t --name=two --link=dirserver:dirserver --link=one:one ubuntu:latest /bin/bash
-sudo docker create -i -t --name=three --link=dirserver:dirserver --link=one:one --link=two:two ubuntu:latest /bin/bash
-sudo docker create -i -t --name=four --link=dirserver:dirserver --link=one:one --link=two:two --link=three:three ubuntu:latest /bin/bash
-sudo docker create -i -t --name=five --link=dirserver:dirserver --link=one:one --link=two:two --link=three:three --link=four:four ubuntu:latest /bin/bash
-sudo docker create -i -t --name=six --link=dirserver:dirserver --link=one:one --link=two:two --link=three:three --link=four:four --link=five:five ubuntu:latest /bin/bash
-sudo docker create -i -t --name=seven --link=dirserver:dirserver --link=one:one --link=two:two --link=three:three --link=four:four --link=five:five --link=six:six ubuntu:latest /bin/bash
+sudo docker create -i -t --cap-add=ALL --name=one --link=dirserver:dirserver ubuntu:latest /bin/bash
+sudo docker create -i -t --cap-add=ALL --name=two --link=dirserver:dirserver --link=one:one ubuntu:latest /bin/bash
+sudo docker create -i -t --cap-add=ALL --name=three --link=dirserver:dirserver --link=one:one --link=two:two ubuntu:latest /bin/bash
+sudo docker create -i -t --cap-add=ALL --name=four --link=dirserver:dirserver --link=one:one --link=two:two --link=three:three ubuntu:latest /bin/bash
+sudo docker create -i -t --cap-add=ALL --name=five --link=dirserver:dirserver --link=one:one --link=two:two --link=three:three --link=four:four ubuntu:latest /bin/bash
+sudo docker create -i -t --cap-add=ALL --name=six --link=dirserver:dirserver --link=one:one --link=two:two --link=three:three --link=four:four --link=five:five ubuntu:latest /bin/bash
+sudo docker create -i -t --cap-add=ALL --name=seven --link=dirserver:dirserver --link=one:one --link=two:two --link=three:three --link=four:four --link=five:five --link=six:six ubuntu:latest /bin/bash
 
 # now we will need to go in and edit /etc/hosts so everyone knows about everyone else
 # since we need a complete topology.
