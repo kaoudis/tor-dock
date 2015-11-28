a068487b8e7bcc5957b33d4313841212912e6a2e
Kelly Nicole Kaoudis <kaoudis@colorado.edu>
2015-11-28 15:53:08 -0700

    update setup

--- setup-container.sh
+++ setup-container.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-yum update && yum install epel-release && yum install vim && yum install tor
-chown root /var/lib/tor
-tor --list-fingerprint --orport 1 \
-    --dirserver "x 127.0.0.1:1 ffffffffffffffffffffffffffffffffffffffff" \
+apt-get update && apt-get upgrade && apt-get install vim && apt-get install tor &&
+chown root /var/lib/tor && mkdir /var/lib/tor/keys
+
+# edit torrc before generating router keys
+
+tor --list-fingerprint --orport 9050 \
+    --dirserver "x <dirserver ip>:9051 <certificate fingerprint of directory server>" \
     --datadirectory /var/lib/tor/
+
+# add environmental variables and /etc/hosts entries for other containers
+
+# set up a tc/qdisc rule, examples of which can be seen in netem-rule, probably including 
+# network delays and/or corruption
+
+# start tor (make sure dirserver is up, then add relays to ad hoc network)
+
+
